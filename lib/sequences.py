6a2f98b7d2a9f09ba1a4302b6a2f233c914682ba
NoniMuthoni <noni.muthoni.noni@gmail.com>
2023-11-23 19:55:45 +0300

    it works

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    n1, n2 = 0, 1
+    n3 = 0
+    print ("Fibonacci series:{}, {}".format(n1,n2), end=" ")
+    for i in range(length):
+        n3 = n1 +n2
+        print("{},".format(n3), end=" ")
+
+        n1= n2
+        n2= n3
+print_fibonacci()
